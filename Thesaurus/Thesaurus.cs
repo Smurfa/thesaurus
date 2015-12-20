6173000851ca0bc7e5a35631c61417549b8a1912
Smurfa <pontus.franzen@live.se>
2015-12-20 14:39:07 +0100

    Find synonyms to word works better, need to fix add synonyms.

--- Thesaurus/Thesaurus.cs
+++ Thesaurus/Thesaurus.cs
@@ -9,24 +9,35 @@ namespace Thesaurus
 
         public void AddSynonyms(IEnumerable<string> synonyms)
         {
-            //Check if any of the words being added already is in the thesaurus
+            //Check if the words being added already is in the thesaurus
             if (_wordList.Any(x => x.Intersect(synonyms).Any()))
             {
-                int index = _wordList.FindIndex(x => x.Intersect(synonyms).Any());
-                _wordList.Insert(index, _wordList.ElementAt(index).Union(synonyms).ToList());
-                _wordList.RemoveAt(index + 1);
+                //Check if an entire sequence in the thesaurus exists in the sequence to be added (that is, the stored sequence is being updated with additional words)
+                //var query = _wordList.All(x => x.Contains(synonyms);
+                if (_wordList.All(x => x.Intersect(synonyms).Any()))
+                {
+                    int index = _wordList.FindIndex(x => x.Intersect(synonyms).Any());
+                    _wordList.Insert(index, _wordList.ElementAt(index).Union(synonyms).ToList());
+                    _wordList.RemoveAt(index + 1);
+                }
+                //Otherwise assume that the added sequence is of different meaning than the existing sequence(s)
+                else
+                {
+                    _wordList.Add(synonyms.ToList());
+                }
+
             }
-            else
+            else//*/
             {
-                //If not, then safely add the entire list as a new entry
+                //If not, then safely add the entire sequence as a new entry
                 _wordList.Add(synonyms.ToList());
             }
         }
 
         public IEnumerable<string> GetSynonyms(string word)
         {
-            //Find the list containing the word, then exclude the word that was used to search on since it looks strange that a word is a synonym to itself
-            return _wordList.FirstOrDefault(x => x.Contains(word)).Except(new[] { word });
+            //Find the lists containing the word, then exclude the word that was used to search on since it looks strange that a word is a synonym to itself
+            return _wordList.Where(x => x.Contains(word)).SelectMany(x => x).Except(new[] { word });
         }
 
         public IEnumerable<string> GetWords()
